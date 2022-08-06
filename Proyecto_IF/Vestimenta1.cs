6872d167652a55ebe024ff81f5020fa9bd4fdb1d
ArielPerez9920 <110736679+ArielPerez9920@users.noreply.github.com>
2022-08-06 13:16:31 -0500

    Update Vestimenta1.cs

--- Proyecto_IF/Vestimenta1.cs
+++ Proyecto_IF/Vestimenta1.cs
@@ -1,15 +1,38 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
-namespace Proyecto_IF
+namespace ProyectoFinal
 {
-    class Vestimenta1
+    public class Vestimenta
     {
-          public string Nombre { set; get; }
-          public string Apellido {set;get;}
-          public string Cedula {set; get;} 
-          public string TipodeVestimenta { set; get; }
-          public string ColordeVestimenta { set; get; }
+        //Datos para el comprador
+        public string Nombre { set; get; }
+        public string Apellido { set; get; }
+        public string Cedula { set; get; }
+
+        //Datos para la vestimenta del comprador
+        public string TipodeVestimenta { set; get; }
+        public string ColordeVestimenta { set; get; }
+        public string MarcadeVestimenta { set; get; }
+        public string TipodeTela { set; get; }
+        public int PreciodeVestimenta { set; get; }
+
+        public Vestimenta()
+        {
+        }
+
+        public Vestimenta(string nombre, string apellido, string cedula, string tipodeVestimenta, 
+            string colordeVestimenta, string marcadeVestimenta, string tipodeTela, int preciodeVestimenta)
+        {
+            Nombre = nombre;
+            Apellido = apellido;
+            Cedula = cedula;
+            TipodeVestimenta = tipodeVestimenta;
+            ColordeVestimenta = colordeVestimenta;
+            MarcadeVestimenta = marcadeVestimenta;
+            TipodeTela = tipodeTela;
+            PreciodeVestimenta = preciodeVestimenta;
+        }
     }
 }

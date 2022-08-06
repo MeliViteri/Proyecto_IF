fe1927e9feadb57fe9294a8d4260e14748e75475
MeliViteri <110273105+MeliViteri@users.noreply.github.com>
2022-08-06 13:27:21 -0500

    Update Vestimentas1.cs

--- Proyecto_IF/Vestimentas1.cs
+++ Proyecto_IF/Vestimentas1.cs
@@ -4,7 +4,43 @@ using System.Text;
 
 namespace Proyecto_IF
 {
-    class Vestimentas1
+    public class Vestimentas1
     {
+        public List<Vestimenta1> Vestimentas = new List<Vestimenta1>();
+        public Vestimentas1()
+        {
+            Vestimentas = new List<Vestimenta1>();
+        }
+        
+        //Listamos los datos
+        public void Agregar(Vestimentas vestimentas)
+        {
+            Vestimentas.Add(vestimentas);
+        }
+        //Nombre del comprador que contenga alguna cadena
+        public List<Vestimenta1> Cadena(string cadena)
+        {
+            List<Vestimenta1> aux = new List<Vestimenta>();
+            foreach (Vestimenta vestimenta in Vestimentas1)
+            {
+                if (vestimenta.Nombre.Contains(cadena))
+                {
+                    aux.Add(vestimenta);
+                }
+            }
+            return aux;
+        }
+        public List<Vestimenta> PreciodeVestimenta()
+        {
+            List<Vestimenta> aux = new List<Vestimenta>();
+            foreach (Vestimenta vestimenta in Vestimentas1)
+            {
+                if (vestimenta.PreciodeVestimenta > 50)
+                {
+                    aux.Add(vestimenta);
+                }
+            }
+            return aux;
+        }
     }
 }
